e8ec8135ed28f627a1a2b9deb971aa68bc8e8931
kauaQuintella <kauaquintella.ba@gmail.com>
2025-09-29 22:45:38 -0300

    Initial Commit - Hello World

---
+++ src/main/java/com/cardgame_distribuited_servers/tec502/Tec502Application.java
@@ -0,0 +1,13 @@
+package com.cardgame_distribuited_servers.tec502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tec502Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tec502Application.class, args);
+	}
+
+}
