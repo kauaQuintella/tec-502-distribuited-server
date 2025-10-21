cdb64294f081944e20edc4d5d5761476ceb9aa07
Kauã Quintella <114306723+kauaQuintella@users.noreply.github.com>
2025-10-21 17:26:01 -0300

    Delete src directory

--- src/main/java/com/cardgame_distribuited_servers/tec502/client/game/utils/message/contents/CommandContent.java
+++
@@ -1,8 +0,0 @@
-package com.cardgame_distribuited_servers.tec502.client.game.utils.message.contents;
-
-public class CommandContent extends Content {
-    private String command;
-    public CommandContent(String command) { this.command = command; }
-    public String getCommand() { return command; }
-    public void setCommand(String command) { this.command = command; }
-}
