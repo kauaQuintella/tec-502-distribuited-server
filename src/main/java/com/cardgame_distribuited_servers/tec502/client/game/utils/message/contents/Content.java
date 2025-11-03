8f30bf74a71cbb02770e8b2431cc46e08e7b4f15
Kauã Quintella <114306723+kauaQuintella@users.noreply.github.com>
2025-11-03 19:07:26 -0300

    Delete src directory

--- src/main/java/com/cardgame_distribuited_servers/tec502/client/game/utils/message/contents/Content.java
+++
@@ -1,8 +0,0 @@
-package com.cardgame_distribuited_servers.tec502.client.game.utils.message.contents;
-
-// Classe abstrata
-public abstract class Content {
-}
-
-
-
--- src/main/java/com/cardgame_distribuited_servers/tec502/client/game/utils/message/contents/PlayerActionContent.java
+++
@@ -1,9 +0,0 @@
-package com.cardgame_distribuited_servers.tec502.client.game.utils.message.contents;
-
-public class PlayerActionContent extends Content {
-    private String action; // "FOGO", "AGUA", "NATUREZA"
-    public PlayerActionContent(String action) { this.action = action; }
-
-    public String getAction() { return action; }
-    public void setAction(String action) { this.action = action; }
-}
