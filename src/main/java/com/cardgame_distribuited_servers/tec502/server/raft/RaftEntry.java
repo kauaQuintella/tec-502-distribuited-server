9770e35cf45e0074b8e2265b3452cba3faaa983e
Kauã Quintella <114306723+kauaQuintella@users.noreply.github.com>
2025-11-10 21:37:14 -0300

    Delete src directory

--- src/main/java/com/cardgame_distribuited_servers/tec502/server/raft/RaftEntry.java
+++
@@ -1,6 +0,0 @@
-package com.cardgame_distribuited_servers.tec502.server.raft;
-
-import com.google.gson.JsonElement;
-
-public record RaftEntry(String type, JsonElement payload) {}
-
--- src/main/java/com/cardgame_distribuited_servers/tec502/server/registry/ServerInfo.java
+++
@@ -1,5 +0,0 @@
-package com.cardgame_distribuited_servers.tec502.server.registry;
-
-//Record para objeto de dados imutável e conciso.
-public record ServerInfo(String serverId, String address, int port) {
-}
